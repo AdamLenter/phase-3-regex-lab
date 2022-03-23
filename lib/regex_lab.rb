2416369ec928abdd1a44b127f91d5d1bc55845ec
Adam <adamlenter1@gmail.com>
2022-03-23 13:24:37 -0400

    commit tree 4b1e319e19aa57fd5cc23b8819ae27475a166dd6

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,25 +1,22 @@
 def starts_with_a_vowel?(word)
-    if(word.match(/\b[AEIOUaeiou]/))
-        true
-    else
-        false
-    end
+    word.match?(/\A[AEIOU]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-    
+    text.scan(/\bun\w+ing\b/i)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match?(/\A[A-Z](\w|\W)+[?!\.]\z/)
 
 end
 
 def valid_phone_number?(phone)
-
+    phone.match?(/(\d{3}-\d{3}-\d{4})|(\d{10})|(\(\d{3}\)\d{3}-\d{4})|(\d{3}\s\d{3}\s\d{4})|(\(\d{3}\)\d{7})/)
 end
 
 puts starts_with_a_vowel?("Adam")
